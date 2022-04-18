675fca999d7a73859b654e2101fb19c4355b03a6
Dan Draper <dan@coderdan.co>
2022-04-19 08:41:37 +1000

    Removed cruft

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-class StashRelation < ActiveRecord::Relation
-  def initialize(klass, table: klass.arel_table, predicate_builder: klass.predicate_builder, values: {})
-    super
-  end
-end
-
 class User < ApplicationRecord
   include ActiveStash::Search
 
@@ -12,9 +6,4 @@ class User < ApplicationRecord
 
   encrypts :first_name #, cipher: MyCipher.new
   encrypts :last_name
-
-  def self.men
-    puts "here #{current_scope}"
-    where(gender: "male")
-  end
 end
