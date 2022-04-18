ec8dade5e3768972775409ee889f4b37bf97416e
Dan Draper <dan@coderdan.co>
2022-04-18 12:49:19 +1000

    More experiments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
-class MyCipher
-  def encrypt(message, key, **options)
-    ActiveRecord::Encryption::Message.new(payload: message).tap do |message|
-      #message.headers.iv = iv
-      #message.headers.auth_tag = cipher.auth_tag
-    end
-  end
-
-  def decrypt(ciphertext, key:)
-    ciphertext
+class StashRelation < ActiveRecord::Relation
+  def initialize(klass, table: klass.arel_table, predicate_builder: klass.predicate_builder, values: {})
+    super
   end
 end
 
 class User < ApplicationRecord
-  include StashIndex
+  include ActiveStash::Search
+
   validates :title, :first_name, :last_name, :email, :dob, :gender, 
     presence: true
 
   encrypts :first_name #, cipher: MyCipher.new
   encrypts :last_name
+
+  def self.men
+    puts "here #{current_scope}"
+    where(gender: "male")
+  end
 end
