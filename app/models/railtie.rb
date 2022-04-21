a103bb0d9073bf9413b13276855d44f98acfa830
Dan Draper <dan@coderdan.co>
2022-04-22 08:44:19 +1000

    Tidy ups

--- app/models/railtie.rb
+++
@@ -1,3 +0,0 @@
-class MyRailtie < Rails::Railtie
-  
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_034329) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_19_094050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_034329) do
     t.string "allergies"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "stash_id"
     t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
